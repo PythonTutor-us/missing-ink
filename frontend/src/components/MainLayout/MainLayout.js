1a2addfbaa88897e4efd4a1382dd85c6ca2f97f2
Lay <tonglay1219@gmail.com>
2018-11-16 20:29:46 -0700

    Add the main layout for the web page

---
+++ frontend/src/components/MainLayout/MainLayout.js
@@ -0,0 +1,174 @@
+import React, { Component } from 'react';
+import classNames from 'classnames';
+import {withStyles} from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import AssessmentIcon from '@material-ui/icons/Assessment';
+import WriterIcon from '@material-ui/icons/Create';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+
+const drawerWidth = 240;
+const styles = theme => ({
+    root: {
+        display: 'flex',
+    },
+    appBar: {
+        zIndex: theme.zIndex.drawer + 1,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+    },
+    appBarShift: {
+        marginLeft: drawerWidth,
+        width: `calc(100% - ${drawerWidth}px)`,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    },
+    menuButton: {
+        marginLeft: 12,
+        marginRight: 36,
+    },
+    hide: {
+        display: 'none',
+    },
+    drawer: {
+        width: drawerWidth,
+        flexShrink: 0,
+        whiteSpace: 'nowrap',
+    },
+    drawerOpen: {
+        width: drawerWidth,
+        transition: theme.transitions.create('width', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    },
+    drawerClose: {
+        transition: theme.transitions.create('width', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+        overflowX: 'hidden',
+        width: theme.spacing.unit * 7 + 1,
+        [theme.breakpoints.up('sm')]: {
+            width: theme.spacing.unit * 9 + 1,
+        },
+    },
+    toolbar: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        padding: '0 8px',
+        ...theme.mixins.toolbar,
+    },
+    content: {
+        flexGrow: 1,
+        padding: theme.spacing.unit * 3,
+    },
+});
+const sideBarOrder = [
+    {
+        name: "Dashboard",
+        src: <AssessmentIcon />
+    },
+    {
+        name: "Writer",
+        src: <WriterIcon />
+    }
+];
+
+class MainLayout extends Component {
+    state = {
+        selectedSection: "Dashbord",
+        open: false,
+    };
+
+    handleDrawerOpen = () => {
+        this.setState({ open: true });
+    };
+
+    handleDrawerClose = () => {
+        this.setState({ open: false });
+    };
+
+    render() {
+        const { classes, theme } = this.props;
+
+        return (
+            <div className={classes.root}>
+                <CssBaseline />
+                <AppBar
+                    position="fixed"
+                    color="primary"
+                    className={classNames(classes.appBar, {
+                        [classes.appBarShift]: this.state.open,
+                    })}
+                >
+                    <Toolbar disableGutters={!this.state.open}>
+                        <IconButton
+                            color="inherit"
+                            aria-label="Open drawer"
+                            onClick={this.handleDrawerOpen}
+                            className={classNames(classes.menuButton, {
+                                [classes.hide]: this.state.open,
+                            })}
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <h2>{this.state.selectedSection}</h2>
+                    </Toolbar>
+                </AppBar>
+                <Drawer
+                    variant="permanent"
+                    className={classNames(classes.drawer, {
+                        [classes.drawerOpen]: this.state.open,
+                        [classes.drawerClose]: !this.state.open,
+                    })}
+                    classes={{
+                        paper: classNames({
+                            [classes.drawerOpen]: this.state.open,
+                            [classes.drawerClose]: !this.state.open,
+                        }),
+                    }}
+                    open={this.state.open}
+                >
+                    <div className={classes.toolbar}>
+                        <IconButton onClick={this.handleDrawerClose}>
+                            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+                        </IconButton>
+                    </div>
+                    <Divider />
+                    <List>
+                        {sideBarOrder.map((element, index) => (
+                            <ListItem button key={element.name}>
+                                <ListItemIcon>
+                                    {element.src}
+                                </ListItemIcon>
+                                <ListItemText primary={element.name} />
+                            </ListItem>
+                        ))}
+                    </List>
+                </Drawer>
+                <main className={classes.content}>
+                    <div className={classes.toolbar} />
+                    <p>I am a p tag</p>
+                </main>
+            </div>
+        );
+    }
+}
+
+export default withStyles(styles, { withTheme: true })(MainLayout);
