2009b88465ba981ebe0377c2c869f52bb7c49f80
Terrel Shumway <gh5323@lernisto.com>
2018-11-16 20:27:45 -0700

    add nltk and more endpoints

--- backend/missingink/app.py
+++ backend/missingink/app.py
@@ -3,4 +3,19 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello Hacker World!"
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User {}'.format( username)
+
+@app.route("/d/<docid>/")
+def doc_edit(docid):
+    pass
+
+@app.route('/d/<docid>/update',methods=["POST","GET"])
+def doc_update(docid):
+    # show the user profile for that user
+    return 'User {}'.format( docid)
+
