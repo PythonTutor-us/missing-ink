02a8d33ed0e7dd93a9ce433bce169ce9a1b0de06
Jonah Merrell <jonahmerrell@gmail.com>
2018-11-17 08:24:07 -0700

    add spelling checker

--- backend/missingink/app.py
+++ backend/missingink/app.py
@@ -2,5 +2,16 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+def index():
+    return "Index Page"
+
+@app.route("/hello")
 def hello():
-    return "Hello World!"
+    return "Hello Cruel World!"
+
+@app.route("/user/<username>")
+def show_user_profile(username):
+    return "User {1} {0} {2}".format(username,"foo",username)
+def show_user_profile2(username):
+    return "User {1} {0} {2}".format(username,"foo",username)
+
