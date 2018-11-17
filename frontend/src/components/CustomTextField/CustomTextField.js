2963ce4c371be97e4c7f966687b10a21544be975
Lay <tonglay1219@gmail.com>
2018-11-17 11:01:04 -0700

    Auto save working

--- frontend/src/components/CustomTextField/CustomTextField.js
+++ frontend/src/components/CustomTextField/CustomTextField.js
@@ -2,40 +2,28 @@ import React, {Component} from 'react';
 import TextField from "@material-ui/core/TextField/TextField";
 import Button from "@material-ui/core/Button/Button";
 
-class CustomTextField extends Component{
-    state = {
-        content: ""
-    };
-
-    handleOnChange = (event) => {
-        this.setState({
-            content: event.target.value
-        })
-    };
-
-    render() {
-        return (
-            <div style={{margin: "10px"}}>
-                <h2 style={{float: "left"}}>{this.props.date}</h2>
-                <TextField
-                    value={this.props.content}
-                    placeholder="Let's start writing!"
-                    multiline={true}
-                    rows={15}
-                    rowsMax={20}
-                    fullWidth={true}
-                    onChange={this.handleOnChange}
-                />
-                <Button
-                    variant="contained"
-                    color="default"
-                    style={{float: "right", marginTop: "10px"}}
-                    onClick={() => this.props.buttonOnClick(this.state.content)}>
-                    Save
-                </Button>
-            </div>
-        );
-    }
+const customTextField = (props) => {
+    return (
+        <div style={{margin: "10px"}}>
+            <h2 style={{float: "left"}}>{props.date}</h2>
+            <TextField
+                value={props.content}
+                placeholder="Let's start writing!"
+                multiline={true}
+                rows={15}
+                rowsMax={20}
+                fullWidth={true}
+                onChange={props.handleOnChange}
+            />
+            <Button
+                variant="contained"
+                color="default"
+                style={{float: "right", marginTop: "10px"}}
+                onClick={() => props.buttonOnClick()}>
+                Save
+            </Button>
+        </div>
+    );
 }
 
-export default CustomTextField;
+export default customTextField;
